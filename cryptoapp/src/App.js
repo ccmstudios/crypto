b86f829317eae62fd9715caa47b7beed094bdbe6
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-10-11 03:52:02 -0400

    banner

--- cryptoapp/src/App.js
+++ cryptoapp/src/App.js
@@ -10,8 +10,6 @@ function App() {
   <img src={logo} className="card-img" id='App-logo' alt="..."/>
   <div className="card-img-overlay">
     <h1 className="card-title">CRYPTOVANIA</h1>
-    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p className="card-text">Last updated 3 mins ago</p>
   </div>
 </div>
   
